53ffac667e3a0cc2ee630a36468104b2a5c9144d
Charlie Tekulve <54796991+chatek87@users.noreply.github.com>
2023-10-11 23:09:32 -0400

    midi_triggers_osc.cpp successfully only plays midi when midi note value == 60 (middle c)

--- myprojects/midi_triggers_osc/midi_triggers_osc.cpp
+++ myprojects/midi_triggers_osc/midi_triggers_osc.cpp
@@ -1,27 +1,120 @@
 #include "daisy_pod.h"
 #include "daisysp.h"
+#include <stdio.h>
+#include <string.h>
 
 using namespace daisy;
 using namespace daisysp;
 
-DaisyPod hw;
+DaisyPod   hw;
+Oscillator osc;
+Svf        filt;
+bool       middleCNoteOn = false; // Flag to track middle C note state
 
-void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
+void AudioCallback(AudioHandle::InterleavingInputBuffer  in,
+                   AudioHandle::InterleavingOutputBuffer out,
+                   size_t                                size)
 {
-	hw.ProcessAllControls();
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
-	}
+    float sig;
+    for(size_t i = 0; i < size; i += 2)
+    {
+        sig = osc.Process();
+        filt.Process(sig);
+        out[i] = out[i + 1] = filt.Low();
+    }
 }
 
+// Typical Switch case for Message Type.
+void HandleMidiMessage(MidiEvent m)
+{
+    switch(m.type)
+    {
+        case NoteOn:
+        {
+            NoteOnEvent p = m.AsNoteOn();
+            char        buff[512];
+            sprintf(buff,
+                    "Note Received:\t%d\t%d\t%d\r\n",
+                    m.channel,
+                    m.data[0],
+                    m.data[1]);
+            hw.seed.usb_handle.TransmitInternal((uint8_t *)buff, strlen(buff));
+            // Check if the incoming note is MIDI note 60 (middle C)
+            if(p.note == 60)
+            {
+                if(p.velocity > 0) // Note is turned on
+                {
+                    osc.SetFreq(mtof(p.note));
+                    osc.SetAmp((p.velocity / 127.0f));
+                    middleCNoteOn = true;
+                }
+                else // Note is turned off
+                {
+                    middleCNoteOn = false;
+                }
+            }
+        }
+        break;
+        case NoteOff:
+        {
+            NoteOffEvent p = m.AsNoteOff();
+            // Check if the incoming note is MIDI note 60 (middle C)
+            if(p.note == 60 && middleCNoteOn)
+            {
+                osc.SetAmp(
+                    0.0f); // Turn off the sound when the middle C note is released
+                middleCNoteOn = false;
+            }
+        }
+        break;
+        case ControlChange:
+        {
+            ControlChangeEvent p = m.AsControlChange();
+            switch(p.control_number)
+            {
+                case 1:
+                    // CC 1 for cutoff.
+                    filt.SetFreq(mtof((float)p.value));
+                    break;
+                case 2:
+                    // CC 2 for res.
+                    filt.SetRes(((float)p.value / 127.0f));
+                    break;
+                default: break;
+            }
+            break;
+        }
+        default: break;
+    }
+}
+
+// Main -- Init, and Midi Handling
 int main(void)
 {
-	hw.Init();
-	hw.SetAudioBlockSize(4); // number of samples handled per callback
-	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
-	hw.StartAdc();
-	hw.StartAudio(AudioCallback);
-	while(1) {}
+    // Init
+    float samplerate;
+    hw.Init();
+    hw.SetAudioBlockSize(4);
+    hw.seed.usb_handle.Init(UsbHandle::FS_INTERNAL);
+    System::Delay(250);
+
+    // Synthesis
+    samplerate = hw.AudioSampleRate();
+    osc.Init(samplerate);
+    osc.SetWaveform(Oscillator::WAVE_POLYBLEP_SAW);
+    filt.Init(samplerate);
+
+    // Start stuff.
+    hw.StartAdc();
+    hw.StartAudio(AudioCallback);
+    hw.midi.StartReceive();
+    for(;;)
+    {
+        hw.midi.Listen();
+        // Handle MIDI Events
+        while(hw.midi.HasEvents())
+        {
+            HandleMidiMessage(hw.midi.PopEvent());
+        }
+    }
 }
